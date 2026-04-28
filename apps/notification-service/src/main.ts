fe974737ddc279cd75f1c353c5ab7fc714829b50
Rick_Sucheazzz <89026909698@mail.ru>
2026-04-28 16:37:45 +0300

    build: notification service turned into microservice

--- apps/notification-service/src/main.ts
+++ apps/notification-service/src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { NotificationServiceModule } from './notification-service.module';
 import { IoAdapter } from '@nestjs/platform-socket.io';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(NotificationServiceModule);
   app.useWebSocketAdapter(new IoAdapter(app));
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'notification-service',
+        brokers: [process.env.KAFKA_BROKER ?? 'localhost:9092'],
+      },
+      consumer: {
+        groupId: 'notification-consumer',
+      },
+    },
+  });
+  await app.startAllMicroservices();
   await app.listen(process.env.NOTIFICATION_PORT ?? 3001);
 }
 void bootstrap();
