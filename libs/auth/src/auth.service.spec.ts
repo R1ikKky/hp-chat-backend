b3c1a71b6c211f039a109e8fdc4dc04c2533c17c
Rick_Sucheazzz <89026909698@mail.ru>
2026-05-31 18:49:17 +0300

    fix(test): provide JwtService mock in AuthService spec

--- libs/auth/src/auth.service.spec.ts
+++ libs/auth/src/auth.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { expect, beforeEach, describe, it, jest } from '@jest/globals';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: JwtService, useValue: { verify: jest.fn() } },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
